c36c05bfd1d033a9679fe93d8bd3ccc7ae0d447b
Sahasragoud <146707817+Sahasragoud@users.noreply.github.com>
2026-06-12 21:12:19 +0530

    Add full pyramid pattern of alphabets

--- Pattern programs/TriangularPatternsOfAlphabets.java
+++ Pattern programs/TriangularPatternsOfAlphabets.java
@@ -48,5 +48,20 @@ class Main {
             } 
             System.out.println();
         }
+
+        //full pyramid
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n - i +1 ; j++){
+                System.out.print("  ");
+            }
+            for(int j = 1; j < i; j++){
+                System.out.print((char)(65 + j - 1) + " ");
+            }
+            for(int j = i ; j >= 1; j--){
+                System.out.print((char)(65 + j - 1) + " ");
+            }
+            System.out.println();
+        }
+        
     }
 }
